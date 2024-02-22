54e387823487bf5d7d300825c45dfef620668ee3
Ryan Su <yuansu.china.work@gmail.com>
2024-02-22 09:45:07 +0800

    fix: block token before token deleted

--- internal/logic/token/delete_token_logic.go
+++ internal/logic/token/delete_token_logic.go
@@ -2,8 +2,13 @@ package token
 
 import (
 	"context"
+	"github.com/suyuan32/simple-admin-common/config"
+	"github.com/suyuan32/simple-admin-common/msg/logmsg"
+	"github.com/suyuan32/simple-admin-member-rpc/ent"
 	"github.com/suyuan32/simple-admin-member-rpc/internal/utils/dberrorhandler"
+	"github.com/suyuan32/simple-admin-member-rpc/internal/utils/entx"
 	"github.com/suyuan32/simple-admin-member-rpc/types/mms"
+	"time"
 
 	"github.com/suyuan32/simple-admin-common/utils/uuidx"
 	"github.com/zeromicro/go-zero/core/logx"
@@ -30,7 +35,29 @@ func NewDeleteTokenLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Delet
 }
 
 func (l *DeleteTokenLogic) DeleteToken(in *mms.UUIDsReq) (*mms.BaseResp, error) {
-	_, err := l.svcCtx.DB.Token.Delete().Where(token.IDIn(uuidx.ParseUUIDSlice(in.Ids)...)).Exec(l.ctx)
+	err := entx.WithTx(l.ctx, l.svcCtx.DB, func(tx *ent.Tx) error {
+		tokenData, err := tx.Token.Query().Where(token.IDIn(uuidx.ParseUUIDSlice(in.Ids)...)).All(l.ctx)
+
+		if err != nil {
+			logx.Errorw(logmsg.DatabaseError, logx.Field("detail", err.Error()))
+			return err
+		} else {
+			for _, v := range tokenData {
+				expiredTime := v.ExpiredAt.Sub(time.Now())
+				if expiredTime > 0 {
+					err = l.svcCtx.Redis.Set(l.ctx, config.RedisTokenPrefix+v.Token, "1", expiredTime).Err()
+					if err != nil {
+						logx.Errorw(logmsg.RedisError, logx.Field("detail", err.Error()))
+						return err
+					}
+				}
+			}
+		}
+
+		_, err = tx.Token.Delete().Where(token.IDIn(uuidx.ParseUUIDSlice(in.Ids)...)).Exec(l.ctx)
+		return err
+	})
+
 	if err != nil {
 		return nil, dberrorhandler.DefaultEntError(l.Logger, err, in)
 	}
