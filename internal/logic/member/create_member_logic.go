94446edd717c39540298510e4e693683b2cc52d3
Ryan Su <yuansu.china.work@gmail.com>
2024-07-29 16:26:53 +0800

    fix: check mobile and email before register

--- internal/logic/member/create_member_logic.go
+++ internal/logic/member/create_member_logic.go
@@ -2,14 +2,16 @@ package member
 
 import (
 	"context"
+
 	"github.com/suyuan32/simple-admin-common/utils/encrypt"
 	"github.com/suyuan32/simple-admin-common/utils/pointy"
-
+	"github.com/suyuan32/simple-admin-member-rpc/ent/member"
 	"github.com/suyuan32/simple-admin-member-rpc/internal/svc"
 	"github.com/suyuan32/simple-admin-member-rpc/internal/utils/dberrorhandler"
 	"github.com/suyuan32/simple-admin-member-rpc/types/mms"
 
 	"github.com/suyuan32/simple-admin-common/i18n"
+	"github.com/zeromicro/go-zero/core/errorx"
 	"github.com/zeromicro/go-zero/core/logx"
 )
 
@@ -28,6 +30,28 @@ func NewCreateMemberLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Crea
 }
 
 func (l *CreateMemberLogic) CreateMember(in *mms.MemberInfo) (*mms.BaseUUIDResp, error) {
+	if in.Mobile != nil {
+		checkMobile, err := l.svcCtx.DB.Member.Query().Where(member.MobileEQ(*in.Mobile)).Exist(l.ctx)
+		if err != nil {
+			return nil, dberrorhandler.DefaultEntError(l.Logger, err, in)
+		}
+
+		if checkMobile {
+			return nil, errorx.NewInvalidArgumentError("login.mobileExist")
+		}
+	}
+
+	if in.Email != nil {
+		checkEmail, err := l.svcCtx.DB.Member.Query().Where(member.EmailEQ(*in.Email)).Exist(l.ctx)
+		if err != nil {
+			return nil, dberrorhandler.DefaultEntError(l.Logger, err, in)
+		}
+
+		if checkEmail {
+			return nil, errorx.NewInvalidArgumentError("login.signupUserExist")
+		}
+	}
+
 	query := l.svcCtx.DB.Member.Create().
 		SetNotNilStatus(pointy.GetStatusPointer(in.Status)).
 		SetNotNilUsername(in.Username).
